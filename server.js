5c91c86348d6c311f9e4255255aeac4321884626
MNatiki <natiart07@gmail.com>
2024-03-09 12:56:45 +0300

    redis

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-import express from 'express'
-import router from './routes/index';
-import { parse } from 'uuid';
+import express from 'express';
+import router from './routes/index.js';
 
 const port = parseInt(process.env.PORT, 10) || 5000;
-
 
 const app = express();
 
@@ -11,7 +9,7 @@ app.use(express.json());
 app.use('/', router);
 
 app.listen(port, () => {
-    console.log(`server running on port ${port}`);
+  console.log(`server running on port ${port}`);
 });
 
 export default app;
