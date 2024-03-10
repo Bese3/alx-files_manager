56f3681f8ead09adfe02ba5a66d2c6d91b300d6f
MNatiki <natiart07@gmail.com>
2024-03-10 10:49:49 +0300

    redis

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,4 +1,5 @@
 import sha1 from 'sha1';
+import { ObjectID } from 'mongodb';
 import Queue from 'bull';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
@@ -7,7 +8,8 @@ const userQueue = new Queue('userQueue', 'redis://127.0.0.1:6379');
 
 class UsersController {
   static postNew(request, response) {
-    const { email, password } = request.body;
+    const { email } = request.body;
+    const { password } = request.body;
 
     if (!email) {
       response.status(400).json({ error: 'Missing email' });
@@ -35,7 +37,27 @@ class UsersController {
         }).catch((error) => console.log(error));
       }
     });
-}
+  }
+
+  static async getMe(request, response) {
+    const token = request.header('X-Token');
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+    if (userId) {
+      const users = dbClient.db.collection('users');
+      const idObject = new ObjectID(userId);
+      users.findOne({ _id: idObject }, (err, user) => {
+        if (user) {
+          response.status(200).json({ id: userId, email: user.email });
+        } else {
+          response.status(401).json({ error: 'Unauthorized' });
+        }
+      });
+    } else {
+      console.log('Hupatikani!');
+      response.status(401).json({ error: 'Unauthorized' });
+    }
+  }
 }
 
 module.exports = UsersController;
